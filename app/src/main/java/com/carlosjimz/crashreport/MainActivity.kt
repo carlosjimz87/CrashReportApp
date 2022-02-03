ee753ecae8227448c9b22dba61872af141fa5fec
carlosjimz87 <carlosjimz87@gmail.com>
2022-02-03 14:50:08 +0100

    customExceptionHandler

--- app/src/main/java/com/carlosjimz/crashreport/MainActivity.kt
+++ app/src/main/java/com/carlosjimz/crashreport/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.carlosjimz.crashreport
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Thread.setDefaultUncaughtExceptionHandler(
+            CustomizedExceptionHandler(
+                Thread.getDefaultUncaughtExceptionHandler(),
+                "/mnt/sdcard/"
+            )
+        )
+
+        // test the crash
+        val nullString = null;
+        println(nullString.toString())
     }
+
 }
